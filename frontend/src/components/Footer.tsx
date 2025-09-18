e654a0ae5625b801f36fde46043c37a19fdd2c86
Parker Bata <pmbata@gmail.com>
2025-09-17 18:20:40 -0700

    footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,15 +1,20 @@
 import React from "react";
+import PeopleWatchingLogoSmall from "../assets/Graphics/PeopleWatchingLogoSmall.png";
 
 export const Footer: React.FC = () => {
   return (
     <footer className="site-footer" role="contentinfo">
       <div className="site-footer__inner">
-        <span className="site-footer__brand">Speed Does America</span>
+        <img
+          src={PeopleWatchingLogoSmall}
+          alt="People Watching"
+          className="site-footer__logo"
+        />
         <span className="site-footer__sep" aria-hidden>
           •
         </span>
         <span className="site-footer__text">
-          © {new Date().getFullYear()} All rights reserved
+          © {new Date().getFullYear()} Speed Does America
         </span>
       </div>
     </footer>
